1e772a44199d6ef0c87bcfd086f4241f4dda2e59
Anton Anikushin <asanikushin@edu.hse.ru>
2020-03-04 22:58:14 +0300

    fix swagger scheme & ui

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,10 @@ from app.common import CustomJSONEncoder
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_swagger_ui import get_swaggerui_blueprint
 
 import os
 import config
 
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-swagger = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Simple shtop REST API"
-    }
-)
 
 # создание экземпляра приложения
 app = Flask(__name__)
@@ -31,4 +21,9 @@ from app.handlers import products_v1, products_v2
 
 app.register_blueprint(products_v1, url_prefix="/products")
 app.register_blueprint(products_v2, url_prefix="/products/v2")
-app.register_blueprint(swagger, url_prefix=SWAGGER_URL)
+
+
+from swagger_ui import api_doc
+working_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(working_dir, './static/swagger.yaml')
+api_doc(app, config_path=config_path, url_prefix='/swagger', title='API doc', editor=True)
