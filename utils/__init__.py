55542becea401ab04e2c20a0f529b0a9978c929b
Anton Anikushin <anton.anikushin@kaspersky.com>
2020-03-29 10:58:23 +0300

    fix utils imports & add waiting for rabbitmq

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,6 @@
-from .checkers import check_model_options
+from .checkers import check_model_options, check_email
 from .errorResponse import create_error_with_status, create_error_response
 from .jsonencoder import CustomJSONEncoder
 from .numerator import Numerator
+from .queues import send_message, wait_connection
+from .times import parse_timedelta
